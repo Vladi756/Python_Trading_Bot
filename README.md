ec71e4b62dcf5fb569fee18593d2c7aae0803e51
Vladi756 <kosticv@lsbu.ac.uk>
2022-10-02 22:28:23 +0100

    Established connection to API

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Python_Trading_Bot
 
-A trading bot which uses the CCXT (CryptoCurrency eXChange Trading Library) library to connect and trade with cryptocurrency. 
+A trading bot which uses the Alpaca trading API to fetch data and execute trades.
 
-## 1. Fetching the Data 
 
-Using the CCXT library, the bot fetches data from the popular cryptocurrency exchange Binance. 
+
+
--- src/trading_bot.py
+++ src/trading_bot.py
@@ -1,4 +1,11 @@
-import time 
-from datetime import datetime
-import pandas as pd
-import numpy as np
+import alpaca_trade_api as tradeapi
+
+# Put your Secure and Public Keys here - generate them at Alpaca paper trading account
+API_KEY = ''
+API_SEC = ''
+
+# Base URL for paper trading (not real money)
+BASE_URL = 'https://paper-api.alpaca.markets'
+
+# Open the API connection -- if you want to tradde with real money, change URL accordingly 
+API = tradeapi.REST(key_id = API_KEY, secret_key= API_SEC, base_url= BASE_URL)
